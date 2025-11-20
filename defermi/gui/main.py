acb12131f16b73ac7910d4ab9f809764ec41ea93
lorenzo-villa-hub <lorenzo.villa.1994@gmail.com>
2025-11-20 17:35:01 +0100

    remove sidebar constraints

--- defermi/gui/main.py
+++ defermi/gui/main.py
@@ -33,16 +33,16 @@ pages_dict = {
 }
 
 
-st.markdown("""
-<style>
-/* Set sidebar max-width */
-[data-testid="stSidebar"] {
-    width: 2000px;
-    min-width: 500px;
-    max-width: 3000px;
-}
-</style>
-""", unsafe_allow_html=True)
+# st.markdown("""
+# <style>
+# /* Set sidebar max-width */
+# [data-testid="stSidebar"] {
+#     width: 2000px;
+#     min-width: 500px;
+#     max-width: 3000px;
+# }
+# </style>
+# """, unsafe_allow_html=True)
 
 
 with st.sidebar:
